db431eb4ef17e916044814ba4182a629b841992a
yuskr <55338324+yuskr@users.noreply.github.com>
2020-11-30 11:51:31 +0800

    Create 113001.继承父类R.java

---
+++ 113001.继承父类R.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class R {
+
+	static void test() {
+		String boy_filePath = "img/boy.gif";
+		String girt_filePath="img/girl.gif";
+		int [][]array2d= {{1,2,3},{4,5,6}};
+		
+		System.out.println("boy_filePath:" + boy_filePath);
+		System.out.println("girt_filePath:" + girt_filePath);
+		
+		
+		for(int i[]:array2d) {
+			for(int j : i) {
+				System.out.print(j + ",");
+			}
+			
+			System.out.print("");
+		}
+		
+		
+	}
+	
+}
