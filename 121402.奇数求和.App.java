8989deef678be5fc946fd5aa2190f24aa478520d
yuskr <55338324+yuskr@users.noreply.github.com>
2020-12-14 21:49:24 +0800

    Create 121402.奇数求和.App.java

---
+++ 121402.奇数求和.App.java
@@ -0,0 +1,8 @@
+public class App2 {
+    public static void main(String[] args) {
+
+        Long ddd= Long.valueOf(2147483647);
+        Long sum = (1+ddd)*ddd/2;
+        System.out.println(sum);
+    }
+}
